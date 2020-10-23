cf91aedc4423d35484fbe78ea968c2287a8a7c3f
Jose Cobo <Jose@MacBook-Pro-de-Jose.local>
2020-10-23 11:11:27 +0200

    leemos valor precio y nombre articulo dle select

--- test/test.js
+++ test/test.js
@@ -4,15 +4,16 @@ const sumPrice = document.getElementById('preu'); // Sumatorio del precio unitar
 const operation = document.getElementById("calcula");  // Boton que lanza la función doMaths
 const units = document.getElementById("unidades"); // Campo number donde se indican las unidades que se van a añadir al carrito
 
-
 // Esta función pinta una opción en el elemento option creando una opción select
 // name = nombre del producto que se va a mostrar
 const addSelectOption = (name, price) => {
     const select = document.createElement('option'); // Elemento a crear de tipo option
     select.innerHTML = name; // Texto de entre los tags <option>
     select.setAttribute("value", price) // Asignamos como atrinuto el precio
+    select.setAttribute("name", name);
     optionList.appendChild(select);  // Añadimos el elemento al select
 }
+
 
 // Esta función lee el arxivo products.json y para cada elemento
 // llamam a la función addSelectOption pasandole el nombre y precio de cada
@@ -31,13 +32,15 @@ const loadProducts = () => {
 }
 
 
-
 // Coge el precio del producto y lo multiplica por las unidades seleccionadas
 const doMaths = () => {
     let price = optionList.value; // Leemos el valor seleccionado en el desplegable
-    console.log(price * units.value);
+    let opt = optionList.options[optionList.selectedIndex];
+
+    console.log(`${units.value} unidades de ${opt.text} cuestan: ${price * units.value}`);
     sumPrice.innerHTML = price * units.value; // Pintamos el resultado en un <p>
 }
 
-loadProducts();
-operation.addEventListener('click', doMaths);
+
+loadProducts(); // Pintamos el selcector en el html con los valores del JSON
+operation.addEventListener('click', doMaths); // Escuchamos el boton calcula prar mostrar el precio total
